d42c2b7cf17193e7b4fc568ac7f61e31293469dd
SangivGiovanni <sangivgiovanni@aol.com>
2020-05-24 13:58:41 +0100

    trying to compile monster and indeed into one scraper

--- Indeed.py
+++ Indeed.py
@@ -1,51 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 
-print("Chose field: ")
-a = input()
-print("Chose location (UK): ")
-b = input()
 
-URLi = "https://www.indeed.co.uk/jobs?q=" + a + "&l=" + b
-pagei = requests.get(URLi)
+def indeed(a, b):
 
-soupi = BeautifulSoup(pagei.content, 'html.parser')
-resulti = soupi.find(id="resultsCol")
+    url_i = "https://www.indeed.co.uk/jobs?q=" + a + "&l=" + b
+    page_i = requests.get(url_i)
 
-jobElementsi = resulti.find_all('div', class_='jobsearch-SerpJobCard')
+    soup_i = BeautifulSoup(page_i.content, 'html.parser')
+    result_i = soup_i.find(id="resultsCol")
 
-for i in jobElementsi:
-    title = i.find('h2', class_='title')
-    company = i.find('span', class_='company')
-    location = i.find('div', class_='location accessible-contrast-color-location')
-    if None in (title, company, location):
-        print(i)
-        continue
-    print(title.text.strip())
-    print(company.text.strip())
-    print(location.text.strip())
-    print()
+    job_elements_i = result_i.find_all('div', class_='jobsearch-SerpJobCard')
 
-
-role = True
-
-while role:
-
-    print("Chose job role: ")
-    i = input()
-    if i == "/exit":
-        break
-
-    myJobsi = resulti.find_all('h2', string=lambda text: i in text.lower())
-    print(len(myJobsi))
-
-    if len(myJobsi) < 1:
-        continue
-    else:
-        print("Search results from Indeed: ")
-        print("\n")
-        for j in myJobsi:
-            link = j.find('a')['href']
-            print(j.text.strip())
-            print(f"Apply here: {link}\n")
-        break
+    for i in job_elements_i:
+        title = i.find('h2', class_='title')
+        company = i.find('span', class_='company')
+        location = i.find('div', class_='location accessible-contrast-color-location')
+        if None in (title, company, location):
+            print(i)
+            print()
+            continue
+        print(title.text.strip())
+        print(company.text.strip())
+        print(location.text.strip())
+        print()
--- WebScraper.py
+++ WebScraper.py
@@ -1,51 +1,34 @@
-import requests
-from bs4 import BeautifulSoup
+from Monster import monster
+from Indeed import indeed
 
 print("Chose field: ")
 a = input()
 print("Chose location (UK): ")
 b = input()
 
-URLm = "https://www.monster.co.uk/jobs/search?q=" + a + "&where=" + b + "&cy=uk&client=power&stpage=1&page=4"
-pagem = requests.get(URLm)
-
-soupm = BeautifulSoup(pagem.content, 'html.parser')
-resultsm = soupm.find(id="SearchResults")
-
-jobElementsm = resultsm.find_all('section', class_='card-content')
-
-for i in jobElementsm:
-    title = i.find('h2', class_='title')
-    company = i.find('div', class_='company')
-    location = i.find('div', class_='location')
-    if None in (title, company, location):
-        print(i)
-        continue
-    print(title.text.strip())
-    print(company.text.strip())
-    print(location.text.strip())
-    print()
+monster(a, b)
+indeed(a, b)
 
 
-role = True
-
-while role:
-
-    print("Chose job role: ")
-    c = input()
-    if c == "/exit":
-        break
-
-    myJobsm = resultsm.find_all('h2', string=lambda text: c in text.lower())
-    print(len(myJobsm))
-
-    if len(myJobsm) < 1:
-        continue
-    else:
-        print("Serch results from Monster: ")
-        print("\n")
-        for j in myJobsm:
-            link = j.find('a')['href']
-            print(j.text.strip())
-            print(f"Apply here: {link}\n")
-        break
+# role = True
+#
+# while role:
+#
+#     print("Chose job role: ")
+#     c = input()
+#     if c == "/exit":
+#         break
+#
+#     myJobs = results.find_all('h2', string=lambda text: c in text.lower())
+#     print(len(myJobs))
+#
+#     if len(myJobs) < 1:
+#         continue
+#     else:
+#         print("Serch results from Monster: ")
+#         print("\n")
+#         for j in myJobs:
+#             link = j.find('a')['href']
+#             print(j.text.strip())
+#             print(f"Apply here: {link}\n")
+#         break
